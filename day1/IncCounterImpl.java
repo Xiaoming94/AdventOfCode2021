9cc0fb6559bd8c724a240ab4f8545d2b9b36fd87
Henry Yang <mail@xiaoming.se>
2021-12-02 18:48:06 +0100

    Add Implementation class for IncCounter

---
+++ day1/IncCounterImpl.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class IncCounterImpl implements IncCounter {
+
+    @Override
+    public int countIncrements(List<Integer> input) {
+        return 0;
+    }
+
+}
--- day1/Problem1.java
+++ day1/Problem1.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem1 {
 	public static void main(String [] args) {
-		System.out.println("Hello World");
+		IncCounter incCounter = new IncCounterImpl();
+		Integer [] a = {1,4,2,4,6,7,8,6,7};
+		List <Integer> inputs = new ArrayList <> (Arrays.asList(a));
+		System.out.println(incCounter.countIncrements(inputs));
+		
 	}
 }
