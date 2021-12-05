b50ebe3ac65f8e1f91a327a1bf2be3e5cd7340b1
Henry Yang <mail@xiaoming.se>
2021-12-05 19:44:01 +0100

    Added functions to move a sub

--- day2/Command.hs
+++ day2/Command.hs
@@ -4,12 +4,17 @@ module Command (
     parseCommand) where
 
 data Command = Forward Integer | Up Integer | Down Integer deriving (Show)
-type SubCoord = (Integer, Integer)
+type SubCoord = (Integer, Integer) --- (Horizontal position, Vertical position)
 
-parseCommand :: Command -> Integer
-parseCommand (Forward steps) = steps
-parseCommand (Up steps)      = steps
-parseCommand (Down steps)    = -steps
+parseCommand :: Command -> SubCoord
+parseCommand (Forward steps) = (steps, 0)
+parseCommand (Up steps)      = (0, steps)
+parseCommand (Down steps)    = (0, -steps)
 
 prodSubCoord :: SubCoord -> Integer
 prodSubCoord (posX, posY) = posX * posY
+
+moveSub :: SubCoord -> Command -> SubCoord
+moveSub (horiPos, vertPos) com = (horiPos + hstep, vertPos + vstep)
+    where
+        (hstep, vstep) = parseCommand com    
