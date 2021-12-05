0302323f8cb6aa5914749758d6054d716222b360
Henry Yang <mail@xiaoming.se>
2021-12-05 14:30:35 +0100

    Started working on Problem 2

---
+++ day1/IncCounterImplProb2.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class IncCounterImplProb2 implements IncCounter{
+
+    @Override
+    public int countIncrements(List<Integer> input) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
---
+++ day1/Problem2.java
@@ -0,0 +1,5 @@
+public class Problem2 {
+    public static void main (String [] args) {
+        System.out.println("Starting with problem 2: " + new IncCounterImplProb2().countIncrements(null));
+    }
+}
