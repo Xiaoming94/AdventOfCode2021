14795bafeed8f1fbb1e3207071e812367d895a77
Henry Yang <mail@xiaoming.se>
2021-12-05 14:55:47 +0100

    Added complete solution to Day 1 problem 2

--- day1/Problem2.java
+++ day1/Problem2.java
@@ -1,13 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 public class Problem2 {
     public static void main (String [] args) {
-        Integer [] numList = {199,200,208,210,200,207,240,269,260,263};
-        List <Integer> inputs = new ArrayList<>(Arrays.asList(numList));
-        IncCounter counter = new IncCounterImplProb2();
-        System.out.println(counter.countIncrements(inputs) == 5);
+        BufferedReader reader;
+		List<Integer> inputList = new ArrayList<>();
+		IncCounter incCounter = new IncCounterImplProb2();
+		try {
+			reader = new BufferedReader(new FileReader("./input.txt"));
+			String line = reader.readLine();
+			while (line != null)
+			{
+				int input = Integer.parseInt(line);
+				inputList.add(input);
+				line = reader.readLine();
+			}
+
+			System.out.println(incCounter.countIncrements(inputList));
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
     }
 }
