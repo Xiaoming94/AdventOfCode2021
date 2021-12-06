f2c1c09a68e128ffe10f402a195badff68abc5da
Henry Yang <mail@xiaoming.se>
2021-12-06 09:44:44 +0100

    added complete funciton for moving sub

--- day2/Command.hs
+++ day2/Command.hs
@@ -8,13 +8,19 @@ type SubCoord = (Integer, Integer) --- (Horizontal position, Vertical position)
 
 parseCommand :: Command -> SubCoord
 parseCommand (Forward steps) = (steps, 0)
-parseCommand (Up steps)      = (0, steps)
-parseCommand (Down steps)    = (0, -steps)
+parseCommand (Up steps)      = (0, -steps)
+parseCommand (Down steps)    = (0, steps) -- Going deeper is the positive direction
 
 prodSubCoord :: SubCoord -> Integer
 prodSubCoord (posX, posY) = posX * posY
 
-moveSub :: SubCoord -> Command -> SubCoord
-moveSub (horiPos, vertPos) com = (horiPos + hstep, vertPos + vstep)
+updateSubCoord :: SubCoord -> Command -> SubCoord
+updateSubCoord (horiPos, vertPos) com = (horiPos + hstep, vertPos + vstep)
     where
-        (hstep, vstep) = parseCommand com    
+        (hstep, vstep) = parseCommand com
+
+readCommandsList :: SubCoord -> [Command] -> SubCoord
+readCommandsList subCoord []                 = subCoord
+readCommandsList subCoord (headCom:commands) = readCommandsList newSubCoord commands
+    where
+        newSubCoord = updateSubCoord subCoord headCom
