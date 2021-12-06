f8c510977c90e203adca084c960425f59a2d3e0e
xiaoming <mail@zyming.me>
2021-12-06 17:26:14 +0100

    Added code to count zeros and ones as a pair and codes to convert list of integers to binaries

--- day3/BinaryPowerReport.hs
+++ day3/BinaryPowerReport.hs
@@ -2,9 +2,31 @@ module BinaryPowerReport (
    Bin 
 ) where
 
+import Data.List
 -- Defining a type for binary numbers
 -- This number is defined backwards because this is easier to work with
 data Bin = LSB | One Bin | Zero Bin deriving Show
+
+exInput = [
+           [0,0,1,0,0]
+          ,[1,1,1,1,0]
+          ,[1,0,1,1,0]
+          ,[1,0,1,1,1]
+          ,[1,0,1,0,1]
+          ,[0,1,1,1,1]
+          ,[0,0,1,1,1]
+          ,[1,1,1,0,0]
+          ,[1,0,0,0,0]
+          ,[1,1,0,0,1]
+          ,[0,0,0,1,0]
+          ,[0,1,0,1,0]
+          ]
+
+toBinary :: (Integral a) => [a] -> Bin
+toBinary []   = LSB
+toBinary (x:xs)
+    | x == 1    = One $ toBinary xs
+    | otherwise = Zero $ toBinary xs
 
 asDigits :: Bin -> Integer
 asDigits bin = asDigits' bin 0 0
@@ -13,6 +35,7 @@ asDigits bin = asDigits' bin 0 0
         asDigits' (One rest) binNum pos  = asDigits' rest (binNum + 10 ^ pos) (pos + 1)
         asDigits' (Zero rest) binNum pos = asDigits' rest binNum (pos + 1)  
 
+-- Function for decoding a binary number
 toInt :: (Integral a) => Bin -> a
 toInt = flip decode 0
     where
@@ -20,6 +43,7 @@ toInt = flip decode 0
         decode (Zero rest) val = decode rest (2 * val)
         decode (One rest)  val = decode rest (2 * val + 1) 
 
+-- Function for counting zeros
 countZeros :: Bin -> Integer
 countZeros = flip countZeros' 0
     where
@@ -27,9 +51,14 @@ countZeros = flip countZeros' 0
         countZeros' (Zero rest) zeros = countZeros' rest (zeros + 1)
         countZeros' (One rest) zeros  = countZeros' rest zeros
 
+-- Function for counting ones
 countOnes :: Bin -> Integer
 countOnes = flip countOnes' 0
     where
         countOnes'  LSB ones        = ones
         countOnes' (Zero rest) ones = countOnes' rest ones 
         countOnes' (One rest) ones  = countOnes' rest (ones + 1)
+
+countOnesAndZeros :: Bin -> (Integer, Integer)
+countOnesAndZeros bin = (countOnes bin, countZeros bin)
+
