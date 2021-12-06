597afcb3e8441626f69ba4cf816d69be370a58da
xiaoming <mail@zyming.me>
2021-12-06 16:32:37 +0100

    Added decoding and reading functions for the binary numbers

--- day3/BinaryPowerReport.hs
+++ day3/BinaryPowerReport.hs
@@ -1,17 +1,21 @@
 module BinaryPowerReport (
-   Bin,
-   BinReport 
+   Bin 
 ) where
 
 -- Defining a type for binary numbers
 -- This number is defined backwards because this is easier to work with
-data Bin = MSB | One Bin | Zero Bin deriving Show
+data Bin = LSB | One Bin | Zero Bin deriving Show
 
 asDigits :: Bin -> Integer
 asDigits bin = asDigits' bin 0 0
     where
-        asDigits' (MSB) binNum _         = binNum
+        asDigits' (LSB) binNum _         = binNum
         asDigits' (One rest) binNum pos  = asDigits' rest (binNum + 10 ^ pos) (pos + 1)
         asDigits' (Zero rest) binNum pos = asDigits' rest binNum (pos + 1)  
 
-type BinReport = [[Integer]]
+toInt :: (Integral a) => Bin -> a
+toInt = flip decode 0
+    where
+        decode LSB val         = val
+        decode (Zero rest) val = decode rest (2 * val)
+        decode (One rest)  val = decode rest (2 * val + 1) 
