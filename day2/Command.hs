1131a7a29c9ce8b9fd147a8a180c2eaa81ef1e15
xiaoming <mail@zyming.me>
2021-12-06 14:25:21 +0100

    Added function solution for problem 2 with example commands

--- day2/Command.hs
+++ day2/Command.hs
@@ -7,6 +7,16 @@ module Command (
 
 data Command = Forward Integer | Up Integer | Down Integer deriving (Show)
 type SubCoord = (Integer, Integer) --- (Horizontal position, Vertical position)
+type SubWithAim = (Integer,Integer,Integer) -- Submarine with AIM
+
+newSubWithAim = (0, 0, 0)
+exCommandList = [ Forward 5
+                , Down 5
+                , Forward 8 
+                , Up 3
+                , Down 8
+                , Forward 2
+                ]
 
 parseCommand :: Command -> SubCoord
 parseCommand (Forward steps) = (steps, 0)
@@ -15,6 +25,9 @@ parseCommand (Down steps)    = (0, steps) -- Going deeper is the positive direct
 
 prodSubCoord :: SubCoord -> Integer
 prodSubCoord (posX, posY) = posX * posY
+
+prodSubCoord2 :: SubWithAim -> Integer
+prodSubCoord2 (posX, posY, _) = posX * posY
 
 updateSubCoord :: SubCoord -> Command -> SubCoord
 updateSubCoord (horiPos, vertPos) com = (horiPos + hstep, vertPos + vstep)
@@ -26,3 +39,12 @@ readCommandsList subCoord []                 = subCoord
 readCommandsList subCoord (headCom:commands) = readCommandsList newSubCoord commands
     where
         newSubCoord = updateSubCoord subCoord headCom
+
+-- Here starts the function for solving problem 2
+processCommandProb2 :: SubWithAim -> Command -> SubWithAim
+processCommandProb2 (horiPos, vertPos, aim) com = (horiPos + hstep, vertPos + vstep, aim + daim) 
+    where
+        (hstep, vstep, daim)                = internProcessCommand com 
+        internProcessCommand (Down step)    = (0, 0, step)
+        internProcessCommand (Up step)      = (0, 0, -step)
+        internProcessCommand (Forward step) = (step, step * aim, 0)
