0bb0418d5d9e9bb5951c4c51036286d44d234c70
xiaoming <mail@zyming.me>
2021-12-06 16:54:36 +0100

    Added functions to count ones and zeros in a binary number

--- day3/BinaryPowerReport.hs
+++ day3/BinaryPowerReport.hs
@@ -19,3 +19,17 @@ toInt = flip decode 0
         decode LSB val         = val
         decode (Zero rest) val = decode rest (2 * val)
         decode (One rest)  val = decode rest (2 * val + 1) 
+
+countZeros :: Bin -> Integer
+countZeros = flip countZeros' 0
+    where
+        countZeros'  LSB zeros        = zeros
+        countZeros' (Zero rest) zeros = countZeros' rest (zeros + 1)
+        countZeros' (One rest) zeros  = countZeros' rest zeros
+
+countOnes :: Bin -> Integer
+countOnes = flip countOnes' 0
+    where
+        countOnes'  LSB ones        = ones
+        countOnes' (Zero rest) ones = countOnes' rest ones 
+        countOnes' (One rest) ones  = countOnes' rest (ones + 1)
