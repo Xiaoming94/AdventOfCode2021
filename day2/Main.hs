c10976a33c8c9db1cd67b17717342a3101e57c66
Henry Yang <mail@xiaoming.se>
2021-12-06 11:00:39 +0100

    Add Main file of day2 problem 1 solution in haskell

---
+++ day2/Main.hs
@@ -0,0 +1,22 @@
+import System.IO
+import Control.Monad
+import Data.List.Split
+import Command
+
+main = do
+    commandStringPairs <- (map words) . lines <$> readFile "./input.txt"
+    let commandsList = processCommandStringList commandStringPairs
+    let moveSub = readCommandsList newSub commandsList
+    print $ prodSubCoord moveSub
+
+newSub = (0,0)  
+
+processCommandStringList :: [[String]] -> [Command]
+processCommandStringList = map processCommandStringPair
+    where
+        processCommandStringPair [comType, stepChar]
+            | comType == "forward" = Forward stepNum
+            | comType == "up"      = Up stepNum
+            | comType == "down"    = Down stepNum
+            where
+                stepNum = read stepChar :: Integer
